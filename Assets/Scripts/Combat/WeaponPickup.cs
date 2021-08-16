9b4cd8c982de1b2aa49dc2618b6121a3729e457f
beamchristian <beamchristian@yahoo.com>
2021-08-16 13:03:34 -0400

    updated Weapon Pickup to Respawn after seconds.

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,34 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
         }
 
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+            
+        }
+
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = false;
+            transform.GetChild(0).gameObject.SetActive(shouldShow);
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
+        }
     }
 }
